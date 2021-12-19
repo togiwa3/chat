beb2dd46b79bbc297bc11fc9b9fb48771c4f7517
togiwa3 <togiwa3@gmail.com>
2021-12-19 14:28:24 +0900

    #6_ToDoリストを追加
    ToDoリストを追加する機能を作成

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <section class="container">
     <h1>ToDoList</h1>
     <div>
-      <input type="text" placeholder="Please add Task">
-      <button>追加</button>
+      <input type="text" name="addName" v-model="content" placeholder="Please add Task">
+      <button @click="addTask">Add</button>
     </div>
     <div>
-      <button>ALL</button>
+      <button>All</button>
       <button>ToDo</button>
       <button>Doing</button>
       <button>Done</button>
@@ -21,16 +21,38 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Test</td>
-          <td>2021-12-15 22:00</td>
-          <td><button>ToDo</button></td>
-          <td><button>Delete</button></td>
+        <tr v-for="(item, index) in todos" :key="index">
+          <td>{{ item.content }}</td>
+          <td>{{ item.createdTime }}</td>
+          <td><button>{{ item.status }}</button></td>
+          <td><button>削除</button></td>
         </tr>
       </tbody>
     </table>
   </section>
 </template>
 <script>
-export default {}
+import {mapState} from 'vuex'
+export default {
+  data: function() {
+    return {
+      content: ''
+    }
+  },
+  // todosの中身が変わるたびに描写(vuexからmutationsを呼び出す)
+  computed: {
+    ...mapState(['todos'])
+  },
+  methods: {
+    addTask: function() {
+      // 何かしら入力がある場合はstoreに保存
+      if (this.content != "") {
+        this.$store.commit("addTask", {
+          content: this.content
+        });
+        this.content = "";
+      }
+    }
+  }
+}
 </script>
